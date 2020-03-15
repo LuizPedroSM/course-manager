33fb4aea69dd669a52a6c493d0519960f4de1166
Luiz Pedro <luizpsmoreira@gmail.com>
2020-03-15 02:18:35 -0300

    Add star, pipe to shared

---
+++ src/app/shared/components/star/star.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { StarComponent } from "./star.component";
+
+@NgModule({
+  imports: [],
+  exports: [StarComponent],
+  declarations: [StarComponent],
+  providers: []
+})
+export class StarModule {}
---
+++ src/app/shared/pipe/app-pipe.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { ReplacePipe } from "./replace.pipe";
+
+@NgModule({
+  imports: [],
+  exports: [ReplacePipe],
+  declarations: [ReplacePipe],
+  providers: []
+})
+export class AppPipeModule {}
